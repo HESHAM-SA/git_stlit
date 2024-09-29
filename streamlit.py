f4b10d8b3f3159b1340f2c63e406d1e62c2a869c
root <root@Hesham>
2024-09-29 20:58:17 +0300

    edit04

--- streamlit.py
+++ streamlit.py
@@ -1,13 +1,9 @@
-import streamlit as st
-import joblib
+import streamlit as st 
+import joblip 
 
+st.title('Model to predict a salary')
 
-st.title('Hello world')
-st.write('## Salary')
+model = joblip.load('linear_model.pkl')
 
-x= st.slider('Exp', 0 , 40)
-
-model = joblib.load('linear_model.pkl')
-
+x = st.slider('experiance', 0, 40)
 y = model.predict([[x]])
-st.write('Salary : ',y )
